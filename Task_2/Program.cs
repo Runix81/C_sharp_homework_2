d6f6cae7499f2429806a59ad747afc332d81e4c6
Sergey <runix1981@gmail.com>
2023-02-18 23:38:13 +0300

    add Task show therd figure of number

---
+++ Task_2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру
+//  заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int Prompt(string message)
+{
+    Console.Write($"{message} -> ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void validate()
+{
+    int num = Prompt("Введите число");
+    if (num < 100)
+    {
+        Console.WriteLine("Третьей цыфры нет");
+    }
+    else
+    {
+        while (num > 1000)
+        {
+            num /= 10;
+        }
+        
+        Console.WriteLine($"-> {num % 10}");
+
+    }
+
+}
+
+validate();
+
