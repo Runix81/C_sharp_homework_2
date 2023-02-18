667af32be33f85872540daa9ab86f19cc39a4c9a
Sergey <runix1981@gmail.com>
2023-02-18 23:57:07 +0300

    add task about days of week

---
+++ Task_3/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+int Prompt(string message)
+{
+    Console.Write($"{message} -> ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void Validate()
+{
+    int num = Prompt("Введите цифру дня недели ");
+    if (num > 0 && num < 8)
+    {
+        if (num == 6 || num == 7)
+        {
+            Console.WriteLine("Это выходной");
+        }
+        else
+        {
+            Console.WriteLine("Это рабочий день");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Такого дня недели нет");
+    }
+}
+
+Validate();
